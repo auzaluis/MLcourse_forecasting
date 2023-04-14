30c5ef25b7d9c473817cb7f08d2c0317fe58b4d0
Luis Auza <luis.auza@gmail.com>
2023-04-14 23:56:29 +0000

    WIP

--- 06-script-forecasting.R
+++ 06-script-forecasting.R
@@ -6,3 +6,37 @@ library(dynlm)
 library(lmtest)
 
 load("06-data-forecasting.RData")
+
+# Creando DF de entrenamiento ----
+
+DF.training.diapers <- DF.diapers %>% 
+  filter(periodo <= "2015-12-01")
+
+
+
+# Creando formato TS (time series)
+
+TS.diapers <- ts(data = DF.training.diapers$sales,
+                 start = c(2006,1),
+                 end = c(2015,12),
+                 frequency = 12)
+
+
+
+# Descomposición
+
+plot(TS.diapers)
+
+ggplotly(
+  
+  TS.diapers %>% 
+    decompose(type = "additive") %>% 
+    autoplot()
+  
+)
+
+
+
+
+
+
